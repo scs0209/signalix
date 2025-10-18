3b26be025da049cddf8fa20dda693cee77d77c38
scs0209 <tjdckdtn2463@naver.com>
2025-10-18 17:18:24 +0900

    chore: star.svg 추가가

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -32,7 +32,7 @@ const Layout = async ({ children }: { children: React.ReactNode }) => {
             </div>
             <div className='flex items-center gap-0.5'>
               {[1, 2, 3, 4, 5].map((star) => (
-                <Image src='/assets/icons/star.svg' alt='Star' key={star} width={20} height={20} className='w-5 h-5' />
+                <Image src='star.svg' alt='Star' key={star} width={20} height={20} className='w-5 h-5' />
               ))}
             </div>
           </div>
