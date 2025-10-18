15631613a6e8afb632e67f2c906203065015ad00
scs0209 <tjdckdtn2463@naver.com>
2025-10-18 20:47:05 +0900

    feat: 회원가입 페이지 및 유틸리티 함수 개선
    - 회원가입 페이지에 useRouter 훅 추가로 성공적인 가입 후 리디렉션 기능 구현
    - signUpWithEmail 함수 호출을 활성화하여 사용자 가입 로직 통합
    - 새로운 인기 주식 심볼 목록 추가 및 다양한 유틸리티 함수 구현으로 기능 확장

--- lib/constants.ts
+++ lib/constants.ts
@@ -173,3 +173,65 @@ export const MARKET_DATA_WIDGET_CONFIG = {
     },
   ],
 };
+
+export const POPULAR_STOCK_SYMBOLS = [
+  // Tech Giants (the big technology companies)
+  'AAPL',
+  'MSFT',
+  'GOOGL',
+  'AMZN',
+  'TSLA',
+  'META',
+  'NVDA',
+  'NFLX',
+  'ORCL',
+  'CRM',
+
+  // Growing Tech Companies
+  'ADBE',
+  'INTC',
+  'AMD',
+  'PYPL',
+  'UBER',
+  'ZOOM',
+  'SPOT',
+  'SQ',
+  'SHOP',
+  'ROKU',
+
+  // Newer Tech Companies
+  'SNOW',
+  'PLTR',
+  'COIN',
+  'RBLX',
+  'DDOG',
+  'CRWD',
+  'NET',
+  'OKTA',
+  'TWLO',
+  'ZM',
+
+  // Consumer & Delivery Apps
+  'DOCU',
+  'PTON',
+  'PINS',
+  'SNAP',
+  'LYFT',
+  'DASH',
+  'ABNB',
+  'RIVN',
+  'LCID',
+  'NIO',
+
+  // International Companies
+  'XPEV',
+  'LI',
+  'BABA',
+  'JD',
+  'PDD',
+  'TME',
+  'BILI',
+  'DIDI',
+  'GRAB',
+  'SE',
+];
